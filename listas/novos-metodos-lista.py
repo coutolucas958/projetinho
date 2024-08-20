6009a44e63b2953c46db881f14090f71c83eff6e
lucas <lucas@stoq.com.br>
2024-08-20 19:17:59 -0300

    Inserindo novos dados

--- listas/novos-metodos-lista.py
+++ listas/novos-metodos-lista.py
@@ -17,3 +17,13 @@ print(lista_1) # lista sem o index que você tirou da variavel.
 lista_2 = range(13)
 #ele retorna um objeto do type=range e o list converte para uma lista.
 print(list(lista_2)) 
+
+
+lista_5 = list(range(2, 14, 4))
+# lembrar que incremento não vai até o ultimo numero do range. 
+print(lista_5)
+
+
+len(lista_1)
+###len(precisa de um parametro para ter o que retornar) sorted()
+
